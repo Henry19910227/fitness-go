d05b174df7348dea8c877dda35b494a060e20eae
henry <toyokoyo199@gmail.com>
2021-06-01 20:27:03 +0800

    新增 trainer table 欄位: avatar

--- internal/dto/userdto/trainer.go
+++ internal/dto/userdto/trainer.go
@@ -1,9 +1,10 @@
 package userdto
 
 type Trainer struct {
-	UserID           int64   `json:"user_id" gorm:"column:user_id" example:"1001"`                           // 用戶id
+	UserID           int64   `json:"user_id" gorm:"column:user_id" example:"1001"`                      // 用戶id
 	Name             string  `json:"name" gorm:"column:name" example:"王小明"`                           // 教練本名
 	Nickname         string  `json:"nickname" gorm:"column:nickname" example:"Henry"`                   // 教練暱稱
+	Avatar           string  `json:"avatar" gorm:"column:avatar" example:"ld3ae0faf5we.png"`            // 教練大頭照
 	TrainerStatus    int     `json:"trainer_status" gorm:"column:trainer_status" example:"1"`           // 教練帳戶狀態 (1:正常/2:審核中/3:停權)
 	Email            string  `json:"email" gorm:"column:email" example:"henry@gmail.com"`               // 信箱
 	Phone            string  `json:"phone" gorm:"column:phone" example:"0978820789"`                    // 電話
--- migrations/20210503115315_create_trainers_table.up.sql
+++ migrations/20210503115315_create_trainers_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS trainers (
     `user_id` INT(11) UNSIGNED PRIMARY KEY COMMENT '用戶id',
     `name` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '教練本名',
     `nickname` VARCHAR(20) NOT NULL UNIQUE DEFAULT '' COMMENT '教練暱稱',
+    `avatar` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '教練大頭照',
     `trainer_status` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' COMMENT '教練狀態(1:正常/2:審核中/3:停權)',
     `email` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '信箱',
     `phone` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '電話',
