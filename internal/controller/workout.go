4e85c47100b89e13f3b98a8bf71fd821339d3670
henry <toyokoyo199@gmail.com>
2021-06-20 02:15:25 +0800

    fest(workout controller)：添加 /workout/{workout_id} [DELETE] API
    調用API刪除workout資料

--- internal/controller/workout.go
+++ internal/controller/workout.go
@@ -17,6 +17,7 @@ func NewWorkout(baseGroup *gin.RouterGroup, workoutService service.Workout, user
 	planGroup := baseGroup.Group("/workout")
 	planGroup.Use(userMiddleware)
 	planGroup.PATCH("/:workout_id", workout.UpdateWorkout)
+	planGroup.DELETE("/:workout_id", workout.DeleteWorkout)
 }
 
 // UpdateWorkout 修改訓練
@@ -59,5 +60,34 @@ func (w *workout) UpdateWorkout(c *gin.Context) {
 	w.JSONSuccessResponse(c, workout, "update success!")
 }
 
+// DeleteWorkout 刪除訓練
+// @Summary 刪除訓練
+// @Description 刪除訓練
+// @Tags Workout
+// @Accept json
+// @Produce json
+// @Security fitness_user_token
+// @Param workout_id path int64 true "訓練id"
+// @Success 200 {object} model.SuccessResult{data=workoutdto.WorkoutID} "獲取成功!"
+// @Failure 400 {object} model.ErrorResult "獲取失敗"
+// @Router /workout/{workout_id} [DELETE]
+func (w *workout) DeleteWorkout(c *gin.Context) {
+	var header validator.TokenHeader
+	var uri validator.WorkoutIDUri
+	if err := c.ShouldBindHeader(&header); err != nil {
+		w.JSONValidatorErrorResponse(c, err.Error())
+		return
+	}
+	if err := c.ShouldBindUri(&uri); err != nil {
+		w.JSONValidatorErrorResponse(c, err.Error())
+		return
+	}
+	data, err := w.workoutService.DeleteWorkoutByToken(c, header.Token, uri.WorkoutID)
+	if err != nil {
+		w.JSONErrorResponse(c, err)
+		return
+	}
+	w.JSONSuccessResponse(c, data, "delete success!")
+}
 
 
