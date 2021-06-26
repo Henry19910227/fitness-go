aeadf6e5da18cd2b9bdefee4b56e292ef31892ca
henry <toyokoyo199@gmail.com>
2021-06-27 03:34:40 +0800

    feat(action controller)：新增 /action/{action_id} [PATCH] 修改動作 API

--- internal/controller/action.go
+++ internal/controller/action.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/Henry19910227/fitness-go/internal/dto/actiondto"
 	"github.com/Henry19910227/fitness-go/internal/service"
 	"github.com/Henry19910227/fitness-go/internal/validator"
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,50 @@ func NewAction(baseGroup *gin.RouterGroup, actionService service.Action, userMid
 	action := &Action{actionService: actionService}
 	actionGroup := baseGroup.Group("/action")
 	actionGroup.Use(userMiddleware)
+	actionGroup.PATCH("/:action_id", action.UpdateAction)
 	actionGroup.DELETE("/:action_id", action.DeleteAction)
+}
+
+// UpdateAction 修改動作
+// @Summary 修改動作
+// @Description 修改動作
+// @Tags Action
+// @Accept json
+// @Produce json
+// @Security fitness_user_token
+// @Param action_id path int64 true "動作id"
+// @Param json_body body validator.UpdateActionBody true "輸入參數"
+// @Success 200 {object} model.SuccessResult{data=actiondto.Action} "更新成功!"
+// @Failure 400 {object} model.ErrorResult "更新失敗"
+// @Router /action/{action_id} [PATCH]
+func (a *Action) UpdateAction(c *gin.Context) {
+	var header validator.TokenHeader
+	var uri validator.ActionIDUri
+	var body validator.UpdateActionBody
+	if err := c.ShouldBindHeader(&header); err != nil {
+		a.JSONValidatorErrorResponse(c, err.Error())
+		return
+	}
+	if err := c.ShouldBindUri(&uri); err != nil {
+		a.JSONValidatorErrorResponse(c, err.Error())
+		return
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		a.JSONValidatorErrorResponse(c, err.Error())
+		return
+	}
+	action, err := a.actionService.UpdateActionByToken(c, header.Token, uri.ActionID, &actiondto.UpdateActionParam{
+		Name: body.Name,
+		Category: body.Category,
+		Body: body.Body,
+		Equipment: body.Equipment,
+		Intro: body.Intro,
+	})
+	if err != nil {
+		a.JSONErrorResponse(c, err)
+		return
+	}
+	a.JSONSuccessResponse(c, action, "update success!")
 }
 
 // DeleteAction 刪除動作
