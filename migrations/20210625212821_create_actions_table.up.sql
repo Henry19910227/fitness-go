64c116fcef615569e36a8fbb2a1e9280145dbb47
henry <toyokoyo199@gmail.com>
2021-06-28 20:18:12 +0800

    fix(migrate)：添加 actions table FOREIGN KEY ON DELETE CASCADE 屬性
    添加 "當父表資料刪除時，該表關聯的資料也會一起刪除" 特性

--- migrations/20210625212821_create_actions_table.up.sql
+++ migrations/20210625212821_create_actions_table.up.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS actions (
     `video` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '動作影片',
     `create_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建時間',
     `update_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '更新時間',
-    CONSTRAINT fk_actions_course_id_to_courses_id FOREIGN KEY (course_id) REFERENCES courses(id)
+    CONSTRAINT fk_actions_course_id_to_courses_id FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8mb4 AUTO_INCREMENT = 1;
