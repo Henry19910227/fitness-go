0f670a47b551ab3982670eb8c60904bcb34cdfe9
henry <toyokoyo199@gmail.com>
2021-06-28 20:17:49 +0800

    fix(migrate)：添加 plans table FOREIGN KEY ON DELETE CASCADE 屬性
    添加 "當父表資料刪除時，該表關聯的資料也會一起刪除" 特性

--- migrations/20210615164919_create_plans_table.up.sql
+++ migrations/20210615164919_create_plans_table.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS plans (
     `workout_count` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '訓練數量',
     `create_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建時間',
     `update_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '更新時間',
-    CONSTRAINT fk_plan_course_id_to_courses_id FOREIGN KEY (course_id) REFERENCES courses(id)
+    CONSTRAINT fk_plan_course_id_to_courses_id FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8mb4 AUTO_INCREMENT = 1;
