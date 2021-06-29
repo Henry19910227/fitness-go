0c54dd005f3ac53d25cc601de5f9ee657c25e0c1
henry <toyokoyo199@gmail.com>
2021-06-29 13:11:02 +0800

    refactor(plan service)：重構 plan 更新 & 刪除服務權限驗證功能

--- internal/service/plan.go
+++ internal/service/plan.go
@@ -51,21 +51,16 @@ func (p *plan) CreatePlan(c *gin.Context, courseID int64, name string) (*plandto
 }
 
 func (p *plan) UpdatePlanByToken(c *gin.Context, token string, planID int64, name string) (*plandto.Plan, errcode.Error) {
-	uid, err := p.jwtTool.GetIDByToken(token)
-	if err != nil {
-		return nil, p.errHandler.InvalidToken()
-	}
-	isExist, err := p.planRepo.CheckPlanExistByUID(uid, planID)
-	if err != nil {
-		return nil, p.errHandler.SystemError()
-	}
-	if !isExist {
-		return nil, p.errHandler.PermissionDenied()
+	if err := p.checkPlanOwnerByPlanID(c, token, planID); err != nil {
+		return nil, err
 	}
 	return p.UpdatePlan(c, planID, name)
 }
 
 func (p *plan) UpdatePlan(c *gin.Context, planID int64, name string) (*plandto.Plan, errcode.Error) {
+	if err := p.checkPlanEditableByPlanID(c, planID); err != nil {
+		return nil, err
+	}
 	if err := p.planRepo.UpdatePlanByID(planID, name); err != nil {
 		p.logger.Set(c, handler.Error, "PlanRepo", p.errHandler.SystemError().Code(), err.Error())
 		return nil, p.errHandler.SystemError()
@@ -97,34 +92,47 @@ func (p *plan) GetPlansByCourseID(c *gin.Context, courseID int64) ([]*plandto.Pl
 }
 
 func (p *plan) DeletePlanByToken(c *gin.Context, token string, planID int64) (*plandto.PlanID, errcode.Error) {
-	uid, err := p.jwtTool.GetIDByToken(token)
-	if err != nil {
-		return nil, p.errHandler.InvalidToken()
-	}
-	isExist, err := p.planRepo.CheckPlanExistByUID(uid, planID)
-	if err != nil {
-		return nil, p.errHandler.SystemError()
-	}
-	if !isExist {
-		return nil, p.errHandler.PermissionDenied()
+	if err := p.checkPlanOwnerByPlanID(c, token, planID); err != nil {
+		return nil, err
 	}
 	return p.DeletePlan(c, planID)
 }
 
 func (p *plan) DeletePlan(c *gin.Context, planID int64) (*plandto.PlanID, errcode.Error) {
-	//以planID查詢課表狀態
-	status, err := p.courseRepo.FindCourseStatusByPlanID(planID)
-	if err != nil {
-		p.logger.Set(c, handler.Error, "CourseRepo", p.errHandler.SystemError().Code(), err.Error())
-		return nil, p.errHandler.SystemError()
-	}
-	//課表狀態必須是"準備中"或"被退審"，才允許刪除
-	if !(status == 1 || status == 4) {
-		return nil, p.errHandler.PermissionDenied()
+	if err := p.checkPlanEditableByPlanID(c, planID); err != nil {
+		return nil, err
 	}
 	if err := p.planRepo.DeletePlanByID(planID); err != nil {
 		p.logger.Set(c, handler.Error, "PlanRepo", p.errHandler.SystemError().Code(), err.Error())
 		return nil, p.errHandler.SystemError()
 	}
 	return &plandto.PlanID{ID: planID}, nil
+}
+
+func (p *plan) checkPlanOwnerByPlanID(c *gin.Context, token string, planID int64) errcode.Error {
+	uid, err := p.jwtTool.GetIDByToken(token)
+	if err != nil {
+		return p.errHandler.InvalidToken()
+	}
+	ownerID, err := p.planRepo.FindPlanOwnerByID(planID)
+	if err != nil {
+		p.logger.Set(c, handler.Error, "Plan", p.errHandler.SystemError().Code(), err.Error())
+		return p.errHandler.SystemError()
+	}
+	if ownerID != uid {
+		return p.errHandler.PermissionDenied()
+	}
+	return nil
+}
+
+func (p *plan) checkPlanEditableByPlanID(c *gin.Context, planID int64) errcode.Error {
+	status, err := p.courseRepo.FindCourseStatusByPlanID(planID)
+	if err != nil {
+		p.logger.Set(c, handler.Error, "CourseRepo", p.errHandler.SystemError().Code(), err.Error())
+		return p.errHandler.SystemError()
+	}
+	if !(status == 1 || status == 4) {
+		return p.errHandler.PermissionDenied()
+	}
+	return nil
 }
