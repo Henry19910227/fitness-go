cf66d691714627f5d3ff92a88d8ce8c23b3b6a06
henry <toyokoyo199@gmail.com>
2021-07-06 16:28:41 +0800

    feat(course validator)：將 course 驗證器部分屬性修改為能容納 0 或是 空字串

--- internal/validator/course.go
+++ internal/validator/course.go
@@ -15,14 +15,14 @@ type CreateCourseBody struct {
 }
 
 type UpdateCourseBody struct {
-	Category *int `json:"category" binding:"omitempty,oneof=1 2 3 4 5 6" example:"3"` // 課表類別(1:有氧心肺訓練/2:間歇肌力訓練/3:重量訓練/4:阻力訓練/5:徒手訓練/6:其他)
+	Category *int `json:"category" binding:"omitempty,oneof=0 1 2 3 4 5 6" example:"3"` // 課表類別(0:未指定/1:有氧心肺訓練/2:間歇肌力訓練/3:重量訓練/4:阻力訓練/5:徒手訓練/6:其他)
 	ScheduleType *int `json:"schedule_type" binding:"omitempty,oneof=1 2" example:"2"` // 排課類別(1:單一訓練/2:多項計畫)
-	SaleType *int `json:"sale_type" binding:"omitempty,oneof=1 2 3" example:"2"` // 銷售類型(1:免費課表/2:訂閱課表/3:付費課表)
+	SaleType *int `json:"sale_type" binding:"omitempty,oneof=0 1 2 3" example:"2"` // 銷售類型(0:未指定/1:免費課表/2:訂閱課表/3:付費課表)
 	Price *int64 `json:"price" binding:"omitempty" example:"330"` // 售價
 	Name *string `json:"name" binding:"omitempty,min=1,max=20" example:"Henry課表"` // 課表名稱(1~20字元)
 	Intro *string `json:"intro" binding:"omitempty,max=400" example:"佛系的健身課表"` // 課表介紹(0~400字元)
 	Food *string `json:"food" binding:"omitempty,max=400" example:"多吃雞胸"` // 飲食建議(0~400字元)
-	Level *int `json:"level" binding:"omitempty,oneof=1 2 3 4" example:"3"` // 強度(1:初級/2:中級/3:中高級/4:高級)
+	Level *int `json:"level" binding:"omitempty,oneof=0 1 2 3 4" example:"3"` // 強度(0:未指定/1:初級/2:中級/3:中高級/4:高級)
 	Suit *string `json:"suit" binding:"omitempty,suit,max=5" example:"2,5,7"` // 適用對象(1:女性/2:男性/3:初學者/4:進階者/5:專業/6:長輩/7:運動員/8:孕婦/9:產後/10:其他)
 	Equipment *string `json:"equipment" binding:"omitempty,equipment,max=5" example:"1,2,5"` // 所需器材(1:無需任何器材/2:啞鈴/3:槓鈴/4:固定式器材/5:彈力繩/6:壺鈴/7:訓練椅/8:瑜珈墊/9:其他)
 	Place *string `json:"place" binding:"omitempty,place,max=5" example:"1,2,3"` // 適合場地(1:健身房/2:居家/3:空地/4:戶外/5:其他)
