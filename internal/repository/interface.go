4cc0157416c7ea5b61187a37e632efe7a73dc7f0
henry <toyokoyo199@gmail.com>
2021-07-07 16:38:13 +0800

    refactor(course repo)：刪除沒用到的函數

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -33,14 +33,6 @@ type Course interface {
 	FindCourseByPlanID(planID int64, entity interface{}) error
 	FindCourseByWorkoutID(workoutID int64, entity interface{}) error
 	FindCourseByActionID(actionID int64, entity interface{}) error
-	FindCourseOwnerByID(courseID int64) (int64, error)
-	FindCourseOwnerByPlanID(planID int64) (int64, error)
-	FindCourseOwnerByWorkoutID(workoutID int64) (int64, error)
-	FindCourseOwnerByActionID(actionID int64) (int64, error)
-	FindCourseStatusByID(courseID int64) (int, error)
-	FindCourseStatusByPlanID(planID int64) (int, error)
-	FindCourseStatusByWorkoutID(workoutID int64) (int, error)
-	FindCourseStatusByActionID(actionID int64) (int, error)
 	DeleteCourseByID(courseID int64) error
 }
 
