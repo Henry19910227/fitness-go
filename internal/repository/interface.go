c004d70110b0139354ca7721710c29daf69678d3
henry <toyokoyo199@gmail.com>
2021-07-10 01:01:12 +0800

    feat(workout_set repo)：新增 CreateWorkoutSetsByWorkoutID SQL 函數

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -55,6 +55,7 @@ type Workout interface {
 }
 
 type WorkoutSet interface {
+	CreateWorkoutSetsByWorkoutID(workoutID int64, actionIDs []int64) ([]int64, error)
 	CreateRestSetByWorkoutID(workoutID int64) (int64, error)
 	FindWorkoutSetByID(setID int64) (*model.WorkoutSet, error)
 }
--- internal/repository/workout_set.go
+++ internal/repository/workout_set.go
@@ -15,6 +15,50 @@ func NewWorkoutSet(gorm tool.Gorm) WorkoutSet {
 	return &set{gorm: gorm}
 }
 
+func (s *set) CreateWorkoutSetsByWorkoutID(workoutID int64, actionIDs []int64) ([]int64, error) {
+	sets := make([]*model.WorkoutSet, 0)
+	for _, v := range actionIDs {
+		var actionID = v
+		set := model.WorkoutSet{
+			WorkoutID: workoutID,
+			ActionID: &actionID,
+			Type: 1,
+			AutoNext: "N",
+			Weight: 1,
+			Reps: 1,
+			Distance: 1,
+			Duration: 1,
+			Incline: 1,
+			CreateAt: time.Now().Format("2006-01-02 15:04:05"),
+			UpdateAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+		sets = append(sets, &set)
+	}
+	if err := s.gorm.DB().Transaction(func(tx *gorm.DB) error {
+		//創建訓練組
+		if err := tx.Create(&sets).Error; err != nil {
+			return err
+		}
+		//更新訓練的訓練組個數
+		countQuery := tx.Table("workout_sets").
+			Select("COUNT(*) AS workout_set_count").
+			Where("workout_id = ?", workoutID)
+		if err := tx.Table("workouts").
+			Where("id = ?", workoutID).
+			Update("workout_set_count", countQuery).Error; err != nil {
+				return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	workoutIDs := make([]int64, 0)
+	for _, set := range sets{
+		workoutIDs = append(workoutIDs, set.ID)
+	}
+	return workoutIDs, nil
+}
+
 func (s *set) CreateRestSetByWorkoutID(workoutID int64) (int64, error) {
 	set := model.WorkoutSet{
 		WorkoutID: workoutID,
