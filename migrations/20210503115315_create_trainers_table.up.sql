de77dd23c4bd960881ba3c4b889b393e6907c087
henry <toyokoyo199@gmail.com>
2021-10-04 00:36:56 +0800

    docs(migrate)：修改 trainer 欄位
    修改 address & intro & motto 欄位長度

--- migrations/20210503115315_create_trainers_table.up.sql
+++ migrations/20210503115315_create_trainers_table.up.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS trainers (
     `trainer_status` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' COMMENT '教練狀態(1:正常/2:審核中/3:停權)',
     `email` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '信箱',
     `phone` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '電話',
-    `address` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '地址',
-    `intro` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '教練介紹',
+    `address` VARCHAR(200) NOT NULL DEFAULT '' COMMENT '地址',
+    `intro` VARCHAR(800) NOT NULL DEFAULT '' COMMENT '教練介紹',
     `experience` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '年資',
-    `motto` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '座右銘',
+    `motto` VARCHAR(200) NOT NULL DEFAULT '' COMMENT '座右銘',
     `create_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建時間',
     `update_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '更新時間',
     CONSTRAINT `fk_trainers_user_id_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
