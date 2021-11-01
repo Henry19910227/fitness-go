c485f6a167f0506e26afd7f8581079b9251fe7fe
Henry.Liao <Henry.Liao@skylinetw.com>
2021-11-01 11:05:56 +0800

    docs(migrate)：更新 reviews table
    1. 新增 id 欄位
    2. 新增 review_unique 唯一約束欄位

--- migrations/20211004011052_create_reviews_table.up.sql
+++ migrations/20211004011052_create_reviews_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS reviews (
+    `id` INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '評論id',
     `course_id` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '課表id',
     `user_id` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '用戶id',
     `score` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '分數',
     `body` VARCHAR (400) NOT NULL DEFAULT '' COMMENT '頻論內容',
     `create_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建日期',
-    PRIMARY KEY (`course_id`,`user_id`),
+    UNIQUE KEY `review_unique` (`course_id`,`user_id`),
     CONSTRAINT fk_reviews_course_id_to_courses_id FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
