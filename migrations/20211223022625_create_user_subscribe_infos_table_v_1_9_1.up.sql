f1f5dbfbf30e683277040d5790d9be58ef3e839a
Henry.Liao <Henry.Liao@skylinetw.com>
2022-02-15 11:36:29 +0800

    docs(migrate)： user_subscribe_infos 表添加order_id外鍵欄位

--- migrations/20211223022625_create_user_subscribe_infos_table_v_1_9_1.up.sql
+++ migrations/20211223022625_create_user_subscribe_infos_table_v_1_9_1.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS user_subscribe_infos (
     `user_id`           INT(11) UNSIGNED PRIMARY KEY COMMENT '用戶id',
-    `subscribe_plan_id` INT(11) UNSIGNED COMMENT '訂閱方案id',
+    `order_id`          VARCHAR(20) COMMENT '訂單 id',
     `status`            TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '會員狀態(0:無會員/1:付費會員)',
     `start_date`        DATETIME NOT NULL DEFAULT NOW() COMMENT '訂閱開始日期',
     `expires_date`      DATETIME NOT NULL DEFAULT NOW() COMMENT '訂閱過期日期',
     `update_at`         DATETIME NOT NULL DEFAULT NOW() COMMENT '更新時間',
     CONSTRAINT `fk_subscribe_infos_user_id_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_subscribe_infos_plan_id_to_subscribe_plans_id` FOREIGN KEY (`subscribe_plan_id`) REFERENCES `subscribe_plans` (`id`) ON DELETE CASCADE
+    CONSTRAINT `fk_subscribe_infos_order_id_to_orders_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
