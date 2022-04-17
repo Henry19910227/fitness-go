7406710b50d41cf048f6fc2f307c785d83a8095d
henry <toyokoyo199@gmail.com>
2022-04-17 13:01:24 +0800

    docs(migrate)：新增 max_rm_records、max_reps_records、max_weight_records、max_speed_records、min_duration_records 表

---
+++ migrations/20220417112004_create_max_reps_records_table_v_1_9_5.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS max_reps_records (
+    `id`             INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '最佳紀錄id',
+    `user_id`        INT(11) UNSIGNED COMMENT '用戶id',
+    `action_id`      INT(11) UNSIGNED COMMENT '動作id',
+    `reps`           INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '次數',
+    `update_at`      DATETIME NOT NULL DEFAULT NOW() COMMENT '更新日期',
+    UNIQUE KEY `unique_max_reps_records` (`user_id`,`action_id`),
+    CONSTRAINT fk_max_reps_records_action_id_to_actions_id FOREIGN KEY (action_id) REFERENCES actions(id) ON DELETE CASCADE,
+    CONSTRAINT fk_max_reps_records_user_id_to_users_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT = 1;
