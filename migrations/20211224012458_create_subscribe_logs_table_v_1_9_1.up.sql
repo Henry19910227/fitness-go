eb0e4989cb893054cf42a6d808963fa433e05d53
Henry.Liao <Henry.Liao@skylinetw.com>
2022-04-27 11:16:08 +0800

    docs(migrate)：修改 subscribe_logs 表 original_transaction_id、transaction_id 數據類型

--- migrations/20211224012458_create_subscribe_logs_table_v_1_9_1.up.sql
+++ migrations/20211224012458_create_subscribe_logs_table_v_1_9_1.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS subscribe_logs (
     `id`             INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '訂閱紀錄id',
-    `original_transaction_id` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '初始交易 id',
-    `transaction_id` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '交易 id',
+    `original_transaction_id` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '初始交易 id',
+    `transaction_id` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '交易 id',
     `purchase_date`  DATETIME NOT NULL DEFAULT NOW() COMMENT '訂閱購買日期',
     `expires_date`   DATETIME NOT NULL DEFAULT NOW() COMMENT '訂閱過期日期',
     `type`           VARCHAR(20) NOT NULL DEFAULT '' COMMENT '紀錄類型(初次訂閱:initial_buy/恢復訂閱:resubscribe/續訂:renew/訂閱升級:upgrade/訂閱降級:downgrade/訂閱過期:expired/退費:refund)',
