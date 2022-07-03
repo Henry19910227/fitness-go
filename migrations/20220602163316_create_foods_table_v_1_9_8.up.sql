d4ba4eafe99909e5e36683b9019e79c5eda35ac7
henry <toyokoyo199@gmail.com>
2022-07-03 11:54:14 +0800

    feat(migrate)：新增 foods 表 status 欄位

--- migrations/20220602163316_create_foods_table_v_1_9_8.up.sql
+++ migrations/20220602163316_create_foods_table_v_1_9_8.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS foods (
     `name`                  VARCHAR(50) NOT NULL DEFAULT '' COMMENT '食物名稱',
     `calorie`               INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '食物熱量',
     `amount_desc`           VARCHAR(50) NOT NULL DEFAULT '' COMMENT '份量描述',
+    `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '動作狀態(0:下架/1:上架)',
     `is_deleted`             TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否刪除',
     `create_at`             DATETIME NOT NULL DEFAULT NOW() COMMENT '創建日期',
     `update_at`             DATETIME NOT NULL DEFAULT NOW() COMMENT '更新日期',
