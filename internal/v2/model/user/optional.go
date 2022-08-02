1ca631dc93c6ec76a68819e22beebac1c848d4a8
Henry.Liao <Henry.Liao@skylinetw.com>
2022-08-02 14:58:05 +0800

    fix(user):修正 /v2/user/profile [PATCH] height & weight 參數限制改為 0.1 ~ 999.9

--- internal/v2/model/user/optional.go
+++ internal/v2/model/user/optional.go
@@ -37,10 +37,10 @@ type BirthdayOptional struct {
 	Birthday *string `json:"birthday,omitempty" binding:"omitempty,datetime=2006-01-02" example:"1991-02-27"` // 生日
 }
 type HeightOptional struct {
-	Height *float64 `json:"height,omitempty" binding:"omitempty,min=1,max=250" example:"176.5"` // 身高
+	Height *float64 `json:"height,omitempty" binding:"omitempty,min=0.1,max=999.9" example:"176.5"` // 身高
 }
 type WeightOptional struct {
-	Weight *float64 `json:"weight,omitempty" binding:"omitempty,min=1,max=250" example:"70.5"` // 體重
+	Weight *float64 `json:"weight,omitempty" binding:"omitempty,min=0.1,max=999.9" example:"70.5"` // 體重
 }
 type ExperienceOptional struct {
 	Experience *int `json:"experience,omitempty" binding:"omitempty,oneof=0 1 2 3 4" example:"2"` // 經驗 (0:未指定/1:初學/2:中級/3:中高/4:專業)
