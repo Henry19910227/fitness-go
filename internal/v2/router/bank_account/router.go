a18f9743401f89cdfdb01b0340908d24270068bf
Henry.Liao <Henry.Liao@skylinetw.com>
2022-08-08 10:33:45 +0800

    fix(trainer)：修正 /v2/trainer/bank_account [GET] 圖片連結404問題
    1. 修正文件連結標示錯誤
    2. 修正 Gin StaticFS 尚未配置問題

--- internal/v2/router/bank_account/router.go
+++ internal/v2/router/bank_account/router.go
@@ -7,11 +7,13 @@ import (
 	"github.com/Henry19910227/fitness-go/internal/v2/controller/bank_account"
 	middleware "github.com/Henry19910227/fitness-go/internal/v2/middleware/token"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func SetRoute(v2 *gin.RouterGroup) {
 	controller := bank_account.NewController(orm.Shared().DB())
 	midd := middleware.NewTokenMiddleware(redis.Shared())
+	v2.StaticFS("/resource/trainer/account_image", http.Dir("./volumes/storage/trainer/account_image"))
 	v2.GET("/trainer/bank_account", midd.Verify([]global.Role{global.UserRole}), controller.GetTrainerBankAccount)
 	v2.PATCH("/trainer/bank_account", midd.Verify([]global.Role{global.UserRole}), controller.UpdateTrainerBankAccount)
 }
