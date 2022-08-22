2473cb6fe25d157a8fc68b2cb020807a5092f910
Henry.Liao <Henry.Liao@skylinetw.com>
2022-08-22 16:17:08 +0800

    fix(trainer)：修正 /v2/trainer/profile [GET] 無教練資訊時 回傳 9002 error code

--- internal/v2/repository/trainer/interface.go
+++ internal/v2/repository/trainer/interface.go
@@ -4,6 +4,7 @@ import model "github.com/Henry19910227/fitness-go/internal/v2/model/trainer"
 
 type Repository interface {
 	Find(input *model.FindInput) (output *model.Output, err error)
+	List(input *model.ListInput) (outputs []*model.Output, amount int64, err error)
 	FavoriteList(input *model.FavoriteListInput) (outputs []*model.Output, amount int64, err error)
 	Update(item *model.Table) (err error)
 }
