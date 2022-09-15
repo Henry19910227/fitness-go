b05e407728da0d5d3ebf26bab97dfeccd4038217
Henry.Liao <Henry.Liao@skylinetw.com>
2022-09-15 16:01:56 +0800

    docs(migrate)：新增 user_unsubscribe_monthly_statistics table

---
+++ migrations/20220914202242_create_user_unsubscribe_monthly_statistics_table_v_2_0_13.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS user_unsubscribe_monthly_statistics (
+    `id`        INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '報表id',
+    `year`      INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '年份',
+    `month`     INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '月份',
+    `total`     INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '當月總退訂人數',
+    `male`      INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '男性退訂人數',
+    `female`    INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '女性退訂人數',
+    `age_13_17` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '13-17歲退訂人數',
+    `age_18_24` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '18-24歲退訂人數',
+    `age_25_34` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '25-34歲退訂人數',
+    `age_35_44` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '35-44歲退訂人數',
+    `age_45_54` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '45-54歲退訂人數',
+    `age_55_64` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '55-64歲退訂人數',
+    `age_65_up` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '65+歲退訂人數',
+    `create_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '創建日期',
+    `update_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '更新日期',
+    UNIQUE KEY `unique_user_unsubscribe_monthly_statistics` (`year`, `month`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT = 1;
