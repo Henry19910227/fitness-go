130039a48788c31caa3f7ff66ca7c26b039ae550
Henry.Liao <Henry.Liao@skylinetw.com>
2022-12-21 18:46:58 +0800

    refactor(max_weight_record)：重構 max_weight_record service 的 CreateOrUpdate function
    避免 auto id 一直累加

---
+++ internal/v2/model/max_weight_record/input.go
@@ -0,0 +1,41 @@
+package max_weight_record
+
+import (
+	"github.com/Henry19910227/fitness-go/internal/v2/field/max_weight_record/optional"
+	pagingOptional "github.com/Henry19910227/fitness-go/internal/v2/field/paging/optional"
+	"github.com/Henry19910227/fitness-go/internal/v2/model/join"
+	orderBy "github.com/Henry19910227/fitness-go/internal/v2/model/order_by"
+	"github.com/Henry19910227/fitness-go/internal/v2/model/preload"
+	"github.com/Henry19910227/fitness-go/internal/v2/model/where"
+)
+
+type PagingInput = struct {
+	pagingOptional.PageField
+	pagingOptional.SizeField
+}
+type PreloadInput = preload.Input
+type WhereInput = where.Input
+type JoinInput = join.Input
+type OrderByInput = orderBy.Input
+type CustomOrderByInput = orderBy.CustomInput
+
+type FindInput struct {
+	optional.IDField
+	optional.UserIDField
+	optional.ActionIDField
+	PreloadInput
+	JoinInput
+	WhereInput
+}
+
+type ListInput struct {
+	optional.IDField
+	optional.UserIDField
+	optional.ActionIDField
+	JoinInput
+	WhereInput
+	PagingInput
+	PreloadInput
+	OrderByInput
+	CustomOrderByInput
+}
