4483dd1529b6c992c89b5d68fba586db13e8dd3a
Henry.Liao <Henry.Liao@skylinetw.com>
2022-12-21 15:53:28 +0800

    feat(max_distance_record)：重構 max_distance_record service 的 CreateOrUpdate function
    避免 auto id 一直累加

--- internal/v2/repository/max_distance_record/repositoty.go
+++ internal/v2/repository/max_distance_record/repositoty.go
@@ -1,6 +1,7 @@
 package max_distance_record
 
 import (
+	"fmt"
 	model "github.com/Henry19910227/fitness-go/internal/v2/model/max_distance_record"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -16,6 +17,109 @@ func New(db *gorm.DB) Repository {
 
 func (r *repository) WithTrx(tx *gorm.DB) Repository {
 	return New(tx)
+}
+
+func (r *repository) Find(input *model.FindInput) (output *model.Output, err error) {
+	db := r.db.Model(&model.Output{})
+	// Join
+	if len(input.Joins) > 0 {
+		for _, join := range input.Joins {
+			db = db.Joins(join.Query, join.Args...)
+		}
+	}
+	//加入 id 篩選條件
+	if input.ID != nil {
+		db = db.Where("max_distance_records.id = ?", *input.ID)
+	}
+	// Custom Where
+	if len(input.Wheres) > 0 {
+		for _, where := range input.Wheres {
+			db = db.Where(where.Query, where.Args...)
+		}
+	}
+	//Preload
+	if len(input.Preloads) > 0 {
+		for _, preload := range input.Preloads {
+			db = db.Preload(preload.Field, preload.Conditions...)
+		}
+	}
+	// Select
+	db = db.Select("max_distance_records.*")
+	//查詢數據
+	err = db.First(&output).Error
+	return output, err
+}
+
+func (r *repository) List(input *model.ListInput) (outputs []*model.Output, amount int64, err error) {
+	db := r.db.Model(&model.Output{})
+	// Join
+	if len(input.Joins) > 0 {
+		for _, join := range input.Joins {
+			db = db.Joins(join.Query, join.Args...)
+		}
+	}
+	// id 篩選條件
+	if input.ID != nil {
+		db = db.Where("max_distance_records.id = ?", *input.ID)
+	}
+	// user_id 篩選條件
+	if input.UserID != nil {
+		db = db.Where("max_distance_records.user_id = ?", *input.UserID)
+	}
+	// action 篩選條件
+	if input.ActionID != nil {
+		db = db.Where("max_distance_records.action_id = ?", *input.ActionID)
+	}
+	// Custom Where
+	if len(input.Wheres) > 0 {
+		for _, where := range input.Wheres {
+			db = db.Where(where.Query, where.Args...)
+		}
+	}
+	// Preload
+	if len(input.Preloads) > 0 {
+		for _, preload := range input.Preloads {
+			db = db.Preload(preload.Field, preload.Conditions...)
+		}
+	}
+	// Count
+	db = db.Count(&amount)
+	// Select
+	db = db.Select("max_distance_records.*")
+	// Paging
+	if input.Page != nil && input.Size != nil {
+		db = db.Offset((*input.Page - 1) * *input.Size).Limit(*input.Size)
+	} else if input.Page != nil {
+		db = db.Offset(0)
+	} else if input.Size != nil {
+		db = db.Limit(*input.Size)
+	}
+	// Order
+	if len(input.OrderField) > 0 && len(input.OrderType) > 0 {
+		db = db.Order(fmt.Sprintf("max_distance_records.%s %s", input.OrderField, input.OrderType))
+	}
+	// Custom Order
+	if input.Orders != nil {
+		for _, orderBy := range input.Orders {
+			db = db.Order(orderBy.Value)
+		}
+	}
+	//查詢數據
+	err = db.Find(&outputs).Error
+	return outputs, amount, err
+}
+
+func (r *repository) Create(item *model.Table) (id int64, err error) {
+	err = r.db.Model(&model.Table{}).Create(&item).Error
+	if err != nil {
+		return 0, err
+	}
+	return *item.ID, err
+}
+
+func (r *repository) Update(item *model.Table) (err error) {
+	err = r.db.Model(&model.Table{}).Where("id = ?", *item.ID).Save(item).Error
+	return err
 }
 
 func (r *repository) CreateOrUpdate(item *model.Table) (id *int64, err error) {
