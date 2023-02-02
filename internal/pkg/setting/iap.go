03153258ed6e6899a69c6e9993021e981ad44b20
Henry.Liao <Henry.Liao@skylinetw.com>
2023-02-02 16:36:58 +0800

    fix(setting)：修正 v1 setting

--- internal/pkg/setting/iap.go
+++ internal/pkg/setting/iap.go
@@ -25,40 +25,76 @@ func (i *iap) GetAppServerAPIURL() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.AppStoreServer")
 	}
-	return i.vp.GetString("IAP.Release.AppStoreServer")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.AppStoreServer")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.AppStoreServer")
+	}
+	return ""
 }
 
 func (i *iap) GetPassword() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.Password")
 	}
-	return i.vp.GetString("IAP.Release.Password")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.Password")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.Password")
+	}
+	return ""
 }
 
 func (i *iap) GetKeyPath() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.KeyPath")
 	}
-	return i.vp.GetString("IAP.Release.KeyPath")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.KeyPath")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.KeyPath")
+	}
+	return ""
 }
 
 func (i *iap) GetKeyID() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.KeyID")
 	}
-	return i.vp.GetString("IAP.Release.KeyID")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.KeyID")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.KeyID")
+	}
+	return ""
 }
 
 func (i *iap) GetBundleID() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.BundleID")
 	}
-	return i.vp.GetString("IAP.Release.BundleID")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.BundleID")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.BundleID")
+	}
+	return ""
 }
 
 func (i *iap) GetIssuer() string {
 	if i.mode == "debug" {
 		return i.vp.GetString("IAP.Debug.Issuer")
 	}
-	return i.vp.GetString("IAP.Release.Issuer")
+	if i.mode == "release" {
+		return i.vp.GetString("IAP.Release.Issuer")
+	}
+	if i.mode == "production" {
+		return i.vp.GetString("IAP.Production.Issuer")
+	}
+	return ""
 }
