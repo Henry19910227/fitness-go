6fba87b2f543c7e7bc3ad804dc7694edba375fe8
Henry.Liao <Henry.Liao@skylinetw.com>
2023-03-23 16:08:30 +0800

    feat(user_subscribe_monthly_statistic): 完善 /v2/cms/statistic_monthly/user/subscribe [GET] api 功能

--- internal/v2/resolver/user_subscribe_monthly_statistic/resolver.go
+++ internal/v2/resolver/user_subscribe_monthly_statistic/resolver.go
@@ -19,6 +19,17 @@ func New(statisticService user_subscribe_monthly_statistic.Service) Resolver {
 }
 
 func (r *resolver) APIGetCMSUserSubscribeStatistic(input *api_get_cms_user_subscribe_statistic.Input) (output api_get_cms_user_subscribe_statistic.Output) {
+	currentYear, _ := strconv.Atoi(time.Now().Format("2006"))
+	currentMonth, _ := strconv.Atoi(time.Now().Format("01"))
+	if input.Query.Year > currentYear {
+		output.Set(code.BadRequest, "不可大於當前時間")
+		return output
+	}
+	if input.Query.Year == currentYear && input.Query.Month > currentMonth {
+		output.Set(code.BadRequest, "不可大於當前時間")
+		return output
+	}
+	// 查找是否有統計資料
 	listInput := model.ListInput{}
 	listInput.Month = util.PointerInt(input.Query.Month)
 	listInput.Year = util.PointerInt(input.Query.Year)
@@ -28,23 +39,37 @@ func (r *resolver) APIGetCMSUserSubscribeStatistic(input *api_get_cms_user_subsc
 		return output
 	}
 	data := api_get_cms_user_subscribe_statistic.Data{}
-	data.Year = util.PointerInt(input.Query.Year)
-	data.Month = util.PointerInt(input.Query.Month)
-	data.Total = util.PointerInt(0)
-	data.Male = util.PointerInt(0)
-	data.Female = util.PointerInt(0)
-	data.Age13to17 = util.PointerInt(0)
-	data.Age18to24 = util.PointerInt(0)
-	data.Age25to34 = util.PointerInt(0)
-	data.Age35to44 = util.PointerInt(0)
-	data.Age45to54 = util.PointerInt(0)
-	data.Age55to64 = util.PointerInt(0)
-	data.Age65Up = util.PointerInt(0)
+	// 存在統計資料就 parser
 	if len(statisticOutputs) > 0 {
 		if err := util.Parser(statisticOutputs[0], &data); err != nil {
 			output.Set(code.BadRequest, err.Error())
 			return output
 		}
+		output.Set(code.Success, "success")
+		output.Data = &data
+		return output
+	}
+	// 不存在統計資料就統計一次
+	statisticInput := model.StatisticInput{}
+	statisticInput.Year = input.Query.Year
+	statisticInput.Month = input.Query.Month
+	if err := r.statisticService.Statistic(&statisticInput); err != nil {
+		output.Set(code.BadRequest, err.Error())
+		return output
+	}
+	// 查找
+	findInput := model.FindInput{}
+	findInput.Month = util.PointerInt(input.Query.Month)
+	findInput.Year = util.PointerInt(input.Query.Year)
+	statisticOutput, err := r.statisticService.Find(&findInput)
+	if err != nil {
+		output.Set(code.BadRequest, err.Error())
+		return output
+	}
+	// Parser
+	if err := util.Parser(statisticOutput, &data); err != nil {
+		output.Set(code.BadRequest, err.Error())
+		return output
 	}
 	output.Set(code.Success, "success")
 	output.Data = &data
