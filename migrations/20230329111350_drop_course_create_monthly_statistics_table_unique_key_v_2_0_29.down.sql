d67d0ca1385e4ee4ef4046da7dc59976ade0a644
Henry.Liao <Henry.Liao@skylinetw.com>
2023-03-29 11:21:22 +0800

    docs(migrate)：新增 migrate 腳本，用以修改 course_create_monthly_statistics table
    1. 移除 id 主鍵
    2. 添加 year & month 多重主鍵
    3. 移除原有 year & month unique key

---
+++ migrations/20230329111350_drop_course_create_monthly_statistics_table_unique_key_v_2_0_29.down.sql
@@ -0,0 +1,19 @@
+SET @dbname = "fitness";
+SET @tablename = "course_create_monthly_statistics";
+SET @keyname = "unique_course_create_monthly_statistics";
+SET @preparedStatement = (SELECT IF(
+        (
+            SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+            WHERE
+                (table_name = @tablename)
+                AND (table_schema = @dbname)
+                AND (index_name = @keyname)
+                AND (non_unique = 0)
+        ) > 0,
+        "SELECT 1",
+        CONCAT("ALTER TABLE ", @tablename, " ADD UNIQUE KEY ", @keyname, " (`year`,`month`)")
+    )
+);
+PREPARE addIfNotExists FROM @preparedStatement;
+EXECUTE addIfNotExists;
+DEALLOCATE PREPARE addIfNotExists;
