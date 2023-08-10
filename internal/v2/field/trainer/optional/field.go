b98ac78bfb01f671e155111993ede4f1b63c4ae6
Henry.Liao <Henry.Liao@skylinetw.com>
2023-08-10 10:59:27 +0800

    fix(trainer):修正 /v2/cms/trainers [GET] 使用 user id 作為搜尋條件時，無法搜索的問題
    trainer model 未添加 form tag 導致無法映射 user id value

--- internal/v2/field/trainer/optional/field.go
+++ internal/v2/field/trainer/optional/field.go
@@ -1,7 +1,7 @@
 package optional
 
 type UserIDField struct {
-	UserID *int64 `json:"user_id,omitempty" uri:"user_id" gorm:"column:user_id" binding:"omitempty" example:"10001"` // 用戶id
+	UserID *int64 `json:"user_id,omitempty" form:"user_id" uri:"user_id" gorm:"column:user_id" binding:"omitempty" example:"10001"` // 用戶id
 }
 type NameField struct {
 	Name *string `json:"name,omitempty" form:"name" gorm:"column:name" binding:"omitempty,max=20" example:"亨利"` // 教練本名
